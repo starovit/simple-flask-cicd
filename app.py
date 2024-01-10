42fd0a75f2150cc832c846f6db8f0877888ad8e4
starovit <ivanstarovit@gmail.com>
2024-01-10 13:24:41 +0200

    upd k8s

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Guys!'
+    return 'Hello, Guy!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8050)
+    app.run(host='0.0.0.0', port=80)
